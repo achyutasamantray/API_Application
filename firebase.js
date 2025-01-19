6ccd3cf9ca481e4fe8c59f4ce9ddc75fd2a69c3e
Achyuta Samantaray <ctc.achyuta@gmail.com>
2025-01-19 12:48:39 +0530

    Add files via upload
    The primary goal of this project is to showcase the practical use of cloud computing technologies, specifically focusing on API integration, data processing, and security within a modern application environment. The project leverages Firebase for backend functionalities and incorporates a custom API for secure communication and data handling.

---
+++ firebase.js
@@ -0,0 +1,15 @@
+ var firebaseConfig = {
+    apiKey: "AIzaSyAV05kWHu0oCLafumnhUFrm2MwdU1DcyGc",
+    authDomain: "api-application-d4425.firebaseapp.com",
+    databaseURL: "https://api-application-d4425-default-rtdb.firebaseio.com",
+    projectId: "api-application-d4425",
+    storageBucket: "api-application-d4425.firebasestorage.app",
+    messagingSenderId: "1098147375305",
+    appId: "1:1098147375305:web:6ce020db5d14abf873c518",
+    measurementId: "G-TJB3JYC8RF"
+
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
